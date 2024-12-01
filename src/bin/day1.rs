f03e4e82e39e553d2a996eb6fbcc8e5e88c9f184
y21 <30553356+y21@users.noreply.github.com>
2024-12-01 13:51:01 +0100

    take advantage of the fact that all integers have the same digit count

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,54 +1,79 @@
-use std::cmp::max;
-use std::cmp::min;
+use std::iter;
 
-use itertools::Itertools;
+use aoc24::parse_u32;
+use rustc_hash::FxBuildHasher;
 use rustc_hash::FxHashMap;
+
+const INPUT: &str = include_str!("../../inputs/day1.txt");
+const FULL_INPUT_DIGITS: usize = {
+    // all numbers have the same number of digits,
+    // precompute it here and use a custom u32 parser that takes advantage of it
+    let mut i = 0;
+    let bytes = INPUT.as_bytes();
+    while bytes[i].is_ascii_digit() {
+        i += 1;
+    }
+    i
+};
 
 fn main() {
     let input = include_str!("../../inputs/day1.txt");
 
-    println!("Part 1: {}", part1(input));
-    println!("Part 2: {}", part2(input));
+    println!("Part 1: {}", part1::<FULL_INPUT_DIGITS>(input));
+    println!("Part 2: {}", part2::<FULL_INPUT_DIGITS>(input));
 }
 
-fn iter(input: &str) -> impl Iterator<Item = (u32, u32)> + '_ {
-    input.lines().map(|line| {
-        line.split_ascii_whitespace()
-            .map(|n| n.parse::<u32>().unwrap())
-            .collect_tuple()
-            .unwrap()
+fn inputs<const DIGITS: usize>(input: &str) -> impl Iterator<Item = (u32, u32)> + '_ {
+    let mut line = 0;
+    let mut pos = 0;
+
+    iter::from_fn(move || {
+        if line < 1000 {
+            let left = parse_u32::<DIGITS>(input, &mut pos);
+            pos += 3;
+            let right = parse_u32::<DIGITS>(input, &mut pos);
+            pos += 1;
+            line += 1;
+            Some((left, right))
+        } else {
+            None
+        }
     })
 }
 
-fn part1(input: &str) -> u32 {
-    let (mut left, mut right): (Vec<_>, Vec<_>) = iter(input).unzip();
+fn part1<const DIGITS: usize>(input: &str) -> u32 {
+    let mut left = Vec::with_capacity(1000);
+    let mut right = Vec::with_capacity(1000);
+    inputs::<DIGITS>(input).for_each(|(l, r)| {
+        left.push(l);
+        right.push(r);
+    });
 
     left.sort_unstable();
     right.sort_unstable();
 
     left.into_iter()
         .zip(right)
-        .map(|(l, r)| max(l, r) - min(l, r))
+        .map(|(l, r)| l.abs_diff(r))
         .sum::<u32>()
 }
 
-fn part2(input: &str) -> u32 {
-    let (left, mut right) = iter(input).fold(
-        (Vec::<u32>::new(), FxHashMap::<u32, (u32, u32)>::default()),
-        |(mut left_vec, mut right_map), (left, right)| {
-            left_vec.push(left);
-            right_map.entry(right).or_default().1 += 1;
-            (left_vec, right_map)
-        },
-    );
+fn part2<const DIGITS: usize>(input: &str) -> u32 {
+    let mut left = Vec::with_capacity(1000);
+    let mut right = FxHashMap::<u32, (u32, u32)>::with_capacity_and_hasher(1000, FxBuildHasher);
 
-    for n in left.iter().copied() {
-        right.entry(n).or_default().0 += 1;
+    inputs::<DIGITS>(input).for_each(|(l, r)| {
+        left.push(l);
+        right.entry(r).or_default().1 += 1;
+    });
+
+    for val in left.iter().copied() {
+        right.entry(val).or_default().0 += 1;
     }
 
     right
         .into_iter()
         .fold(0, |p, (digit, (left_count, right_count))| {
-            p + (digit * left_count * right_count)
+            p + digit * left_count * right_count
         })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
-
+/// Parses the next `DIGITS` in the string as a u32
+pub fn parse_u32<const DIGITS: usize>(input: &str, pos: &mut usize) -> u32 {
+    let input = &input.as_bytes()[*pos..*pos + DIGITS];
+    let mut num = 0;
+    for i in (0..DIGITS).rev() {
+        let digit = (input[i] & 0xf) as u32;
+        let mult = 10_u32.pow((DIGITS - i - 1) as u32);
+        num += digit * mult;
+    }
+    *pos += DIGITS;
+    num
+}
